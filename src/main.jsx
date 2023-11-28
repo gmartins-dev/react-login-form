69fc1c948372ec9535d1c8767e850a65355aaaac
g-launchpad <guilherme@nexyield.com>
2023-11-28 21:33:49 +0000

    fix bg-img loading after the form

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+const image = new Image();
+image.src = './assets/imgs/blue-bg.jpg';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
